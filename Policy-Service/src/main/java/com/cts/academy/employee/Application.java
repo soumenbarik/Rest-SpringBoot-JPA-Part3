3e6598cbba79eddee919aedfa3a513d0bfbf3499
354291 <354291@PC307431.AIG-COGNIZANT.COM>
2019-11-18 17:21:31 +0530

    Updated spring-boot version.

--- Policy-Service/src/main/java/com/cts/academy/employee/Application.java
+++ Policy-Service/src/main/java/com/cts/academy/employee/Application.java
@@ -1,8 +1,10 @@
 package com.cts.academy.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
