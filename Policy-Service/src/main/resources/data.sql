94c6cdd3fe2cbd217d5dde9e35dc0cbb3d597dde
354291 <354291@PC307431.AIG-COGNIZANT.COM>
2019-11-18 10:50:41 +0530

    Remove comment part

--- Policy-Service/src/main/resources/data.sql
+++ Policy-Service/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-/** INSERT INTO policy(policy_id, insurer_name, start_date, email, nos_dependent)
+INSERT INTO policy(policy_id, insurer_name, start_date, email, nos_dependent)
 VALUES ('P-235678', 'AIG','05/08/2014', 'amar.sharma@gmail.com', '0');
 
 INSERT INTO policy (policy_id, insurer_name, start_date, email, nos_dependent)
@@ -8,4 +8,4 @@ INSERT INTO policy (policy_id, insurer_name, start_date, email, nos_dependent)
 VALUES ('P-568563', 'Reliance', '05/07/1989','Atanu.Paul@gmail.com', '2');
 
 INSERT INTO policy (policy_id, insurer_name, start_date, email, nos_dependent)
-VALUES ('P-796325', 'Franklin','15/06/1987','Sourav.Barik@gmail.com', '4'); **/
+VALUES ('P-796325', 'Franklin','15/06/1987','Sourav.Barik@gmail.com', '4');
